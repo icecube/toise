54851133c2da25a0fc66ad40ca04916be2bd7f76
Jakob van Santen <jakob.van.santen@desy.de>
2015-07-22 12:35:18 +0000

    Allow PSF and selection efficiency to depend on zenith as well.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@135450 16731396-06f5-0310-8873-f7f720988828

--- effective_areas.py
+++ effective_areas.py
@@ -40,16 +40,51 @@ def load_jvs_mese():
 	return edges, aeff
 
 from scipy import interpolate
+import tables, dashi
 
 class MuonSelectionEfficiency(object):
-	def __init__(self):
-		f = numpy.load(os.path.join(data_dir, 'veto', 'aachen_muon_selection_efficiency.npz'))
-		self.interp = interpolate.interp1d(f['log_energy'], f['efficiency'],
-		    bounds_error=False, fill_value=0.)
-	def __call__(self, muon_energy):
-		return self.interp(numpy.log10(muon_energy))
+	def __init__(self, filename='aachen_muon_selection_efficiency.npz'):
+		if not filename.startswith('/'):
+			filename = os.path.join(data_dir, 'selection_efficiency', filename)
+		if filename.endswith('.npz'):
+			f = numpy.load(filename)
+			self.interp = interpolate.interp1d(f['log_energy'], f['efficiency'],
+			    bounds_error=False, fill_value=0.)
+		elif filename.endswith('.hdf5'):
+			with tables.open_file(filename) as f:
+				generated = dashi.histload(f, '/generated')
+				detected = dashi.histload(f, '/detected')
+			sp = dashi.histfuncs.histratio(detected.project([0]), generated.project([0]))
+	
+			edges = numpy.concatenate((sp.x - sp.xerr, [sp.x[-1] + sp.xerr[-1]]))
+			loge = 0.5*(numpy.log10(edges[1:]) + numpy.log10(edges[:-1]))
+	
+			loge = numpy.concatenate((loge, loge + loge[-1] + numpy.diff(loge)[0]))
+			v = numpy.concatenate((sp.y, sp.y[-5:].mean()*numpy.ones(sp.y.size)))
+			w = 1/numpy.concatenate((sp.yerr, 1e-2*numpy.ones(sp.yerr.size)))
+			w[~numpy.isfinite(w)] = 1
+	
+			self.interp = interpolate.UnivariateSpline(loge, v, w=w)
+
+
+	def __call__(self, muon_energy, cos_theta):
+		return numpy.clip(self.interp(numpy.log10(muon_energy)), 0, 1)
+
+class ZenithDependentMuonSelectionEfficiency(object):
+	def __init__(self, filename='sunflower_200m_bdt0_efficiency.fits'):
+		from icecube.photospline import I3SplineTable
+		if not filename.startswith('/'):
+			filename = os.path.join(data_dir, 'selection_efficiency', filename)
+		self._spline = I3SplineTable(filename)
+		self.eval = numpy.vectorize(self._eval)
+	def _eval(self, loge, cos_theta):
+		return self._spline.eval([loge, cos_theta])
+	def __call__(self, muon_energy, cos_theta):
+		loge, cos_theta = numpy.broadcast_arrays(numpy.log10(muon_energy), cos_theta)
+		return numpy.clip(self.eval(numpy.log10(muon_energy), cos_theta), 0, 1)
 
 def create_throughgoing_aeff(energy_resolution=0.25,
+    selection_efficiency=MuonSelectionEfficiency(),
     full_sky=False, energy_threshold_scale=1., surface=Cylinder()):
 	"""
 	Create effective areas in the same format as above
@@ -70,21 +105,16 @@ def create_throughgoing_aeff(energy_resolution=0.25,
 		efficiency = dashi.histload(hdf, '/muon_efficiency')
 	
 	# Step 2: Geometric muon effective area (no selection effects yet)
-	# FIXME: assumes cylindrical symmetry. either average out or add explicitly
-	aeff = efficiency.bincontent * (surface.area(efficiency.bincenters[1], numpy.nan)[None,:,None])
+	# FIXME: assumes cylindrical symmetry.
+	aeff = efficiency.bincontent * (numpy.vectorize(surface.average_area)(efficiency.binedges[1][:-1], efficiency.binedges[1][1:])[None,:,None])
 	
 	# Step 3: apply selection efficiency
-	selection_efficiency = MuonSelectionEfficiency()
-	if full_sky:
-		# accept everything
-		aeff *= selection_efficiency(efficiency.bincenters[2]/energy_threshold_scale)[None,None,:]
-	else:
-		# only accept events below the horizon
-		eff = numpy.zeros(aeff.shape[1:])
-		mask = efficiency.bincenters[1] < 0
-		eff[mask,:] = selection_efficiency(efficiency.bincenters[2]/energy_threshold_scale)[None,:]
-		# eff[mask,:] = 1.
-		aeff *= eff[None,:,:]
+	emu, cos_theta = numpy.meshgrid(efficiency.bincenters[2]/energy_threshold_scale, efficiency.bincenters[1], indexing='ij')
+	selection_efficiency = selection_efficiency(emu, cos_theta).T
+	if not full_sky:
+		# only accept events from below the horizon
+		selection_efficiency[cos_theta.T > 0] = 0
+	aeff *= selection_efficiency[None,:,:]
 	
 	# Step 4: apply smearing for energy resolution
 	binwidth = numpy.log10(efficiency.binedges[2][1]/efficiency.binedges[2][0])
