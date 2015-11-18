aa8cebdaa9a7b2fdb9c2727b5430ad629c726539
Jakob van Santen <jakob.van.santen@desy.de>
2015-11-18 13:27:36 +0000

    Add rudimentary documentation
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@139634 16731396-06f5-0310-8873-f7f720988828

---
+++ resources/docs/plots/selection_efficiency.py
@@ -0,0 +1,42 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.gridspec import GridSpec
+from mpl_toolkits.axes_grid.anchored_artists import AnchoredText
+from icecube.gen2_analysis import plotting, effective_areas
+
+configs = [
+    ('IceCube', None),
+    ('EdgeWeighted', 240),
+    ('Sunflower', 200),
+    ('Sunflower', 240),
+    ('Sunflower', 300),
+]
+
+with plotting.pretty():
+    fig = plt.figure(figsize=(8,4))
+    griddy = GridSpec(1, 2)
+    emu = np.logspace(2, 6, 101)
+    ax1, ax2 = (plt.subplot(griddy[i]) for i in xrange(2))
+    for i, (geo, spacing) in enumerate(configs):
+        mueff = effective_areas.get_muon_selection_efficiency(geo, spacing)
+        
+        if spacing is None:
+            kwargs = dict(color='k', label=geo)
+        else:
+            kwargs = dict(label='%s %dm' % (geo, spacing))
+        
+        ax1.semilogx(emu, mueff(emu, 0), **kwargs)
+        ax2.semilogx(emu, mueff(emu, -0.5), **kwargs)
+
+    ax1.add_artist(AnchoredText(r'$\cos\theta=0$', loc=4, frameon=False))
+    ax2.add_artist(AnchoredText(r'$\cos\theta=-0.5$', loc=4, frameon=False))
+        
+    ax1.legend(loc='best')
+    ax1.set_ylabel('Selection efficiency')
+    for ax in fig.axes:
+        ax.set_ylim((0, 1))
+        ax.set_xlabel(r'$E_{\mu}$ [GeV]')
+        ax.grid()
+    plt.tight_layout()
+    plt.show()
