77f7ff8323121fe388b25e727216125f54c436bf
jvansanten <jvansanten@16731396-06f5-0310-8873-f7f720988828>
2016-08-25 14:26:52 +0000

    Expose command-line arguments
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@149569 16731396-06f5-0310-8873-f7f720988828

--- resources/docs/plots/ps_sensitivity_demo.py
+++ resources/docs/plots/ps_sensitivity_demo.py
@@ -4,14 +4,37 @@ import numpy as np
 from matplotlib.gridspec import GridSpec
 from mpl_toolkits.axes_grid.anchored_artists import AnchoredText
 from icecube.gen2_analysis import plotting, effective_areas, diffuse, pointsource, multillh
-
-def create_aeff(geo, spacing, veto_threshold=np.inf, **kwargs):
+from icecube.gen2_analysis import angular_resolution, energy_resolution
+    
+def create_aeff(geometry='IceCube', spacing=200,
+    veto_threshold=1e4, veto_area=0., energy_threshold=None, no_cuts=False,
+    psf_spacing=None, angular_resolution_scale=1., **kwargs):
+    
+    if psf_spacing is None:
+        psf_spacing = spacing
+    
+    if veto_area > 0:
+        kwargs['veto_coverage'] = surface_veto.GeometricVetoCoverage(geometry, spacing, veto_area)
+    
+    if energy_threshold is not None:
+        seleff = effective_areas.get_muon_selection_efficiency(geometry, spacing, energy_threshold=energy_threshold)
+    else:
+        seleff = effective_areas.get_muon_selection_efficiency(geometry, spacing)
+    if no_cuts:
+        selection_efficiency = lambda emu, cos_theta: seleff(emu, cos_theta=0)
+        # selection_efficiency = lambda emu, cos_theta: np.ones(emu.shape)
+        # selection_efficiency = effective_areas.get_muon_selection_efficiency("IceCube", None)
+    else:
+        selection_efficiency = seleff
+    
     return effective_areas.create_throughgoing_aeff(
-        energy_resolution=effective_areas.get_energy_resolution(geo, spacing),
-        selection_efficiency=effective_areas.get_muon_selection_efficiency(geo, spacing),
-        surface=effective_areas.get_fiducial_surface(geo, spacing),
+        energy_resolution=energy_resolution.get_energy_resolution(geometry, spacing),
+        selection_efficiency=selection_efficiency,
+        surface=effective_areas.get_fiducial_surface(geometry, spacing),
         energy_threshold=effective_areas.StepFunction(veto_threshold, 90),
-        psf=effective_areas.get_angular_resolution(geo, spacing),
+        psf=angular_resolution.get_angular_resolution(geometry, psf_spacing, scale=angular_resolution_scale),
+        psi_bins=np.sqrt(np.linspace(0, np.radians(2)**2, 100)),
+        # psi_bins=(np.linspace(0, np.radians(2), 800)),
         **kwargs)
 
 def ps_components(aeff, livetime=1., sindec=0):
@@ -31,10 +54,10 @@ def ps_components(aeff, livetime=1., sindec=0):
     ps = pointsource.SteadyPointSource(aeff, livetime, zenith_bin=zi)
     return ps, diffuse_components, fixed
 
-def demo_plot(geo='Sunflower', spacing=240, sindec=0.1, livetime=1.):
+def demo_plot(geo='Sunflower', spacing=240, sindec=0.1, livetime=1., angular_resolution_scale=1.):
     from matplotlib import cm
     
-    aeff = create_aeff(geo, spacing)
+    aeff = create_aeff(geo, spacing, angular_resolution_scale=angular_resolution_scale)
     ps, background, fixed = ps_components(aeff, livetime=livetime, sindec=sindec)
     
     dp = pointsource.discovery_potential(ps, background, **fixed)
@@ -46,11 +69,15 @@ def demo_plot(geo='Sunflower', spacing=240, sindec=0.1, livetime=1.):
         ax = plt.subplot(griddy[0])
         psi = np.degrees(aeff.bin_edges[-1][:-1])
         bin_area = np.pi*np.diff(psi**2)[0]
-        ax.plot(*plotting.stepped_path(psi, dp*ps.expectations()['tracks'].sum(axis=0)), label=r'$%.1f \times 10^{-12} \rm \,\, TeV \, cm^{-2} \, s^{-1}$' % dp)
+        ax.plot(*plotting.stepped_path(psi, dp*ps.expectations()['tracks'].sum(axis=0)), label=r'$%.2f \times 10^{-12} \rm \,\, TeV \, cm^{-2} \, s^{-1}$' % dp)
         ax.plot(*plotting.stepped_path(psi, background['atmo'].expectations['tracks'].sum(axis=0)), label='Atmos. background')
         ax.plot(*plotting.stepped_path(psi, background['astro'].expectations(gamma=fixed['gamma'])['tracks'].sum(axis=0)), label='Astro. background')
-
-
+        
+        bkg = background['atmo'].expectations['tracks'].sum(axis=0) + \
+            background['astro'].expectations(gamma=fixed['gamma'])['tracks'].sum(axis=0)
+        sig = dp*ps.expectations()['tracks'].sum(axis=0)
+        ax.plot(*plotting.stepped_path(psi, bkg+sig), color='grey')
+        
         ax.set_xlabel('Angular distance to source $\Psi$ [deg]')
         ax.set_ylabel('Mean events/livetime (%.1f bins/sqdeg)' % (1./bin_area))
         ax.legend(title=r'Rates at $\sin\delta=%.1f$' % sindec)
@@ -76,3 +103,20 @@ def demo_plot(geo='Sunflower', spacing=240, sindec=0.1, livetime=1.):
     
         plt.tight_layout() 
 
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--geometry", choices=('IceCube', 'Sunflower', 'EdgeWeighted'), default='Sunflower')
+    parser.add_argument("--spacing", type=int, default=240)
+    parser.add_argument("--veto-area", type=float, default=25)
+    parser.add_argument("--veto-threshold", type=float, default=1e4)
+    parser.add_argument("--no-cuts", default=False, action="store_true")
+    parser.add_argument("--livetime", type=float, default=10.)
+    parser.add_argument("--energy-threshold", type=float, default=None)
+    parser.add_argument("--sin-dec", type=float, default=0.)
+    parser.add_argument("--angular-resolution-scale", type=float, default=1.)
+    opts = parser.parse_args()
+    
+    import pylab
+    demo_plot(geo=opts.geometry, spacing=opts.spacing, sindec=opts.sin_dec, livetime=opts.livetime, angular_resolution_scale=opts.angular_resolution_scale)
+    pylab.show()
