8ffe68dcb28d3a3260a99e9fed93ca9856e9deb2
jvansanten <jvansanten@16731396-06f5-0310-8873-f7f720988828>
2017-01-06 13:59:08 +0000

    Add ARA support from Tianlu
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@152491 16731396-06f5-0310-8873-f7f720988828

--- python/util.py
+++ python/util.py
@@ -1,5 +1,6 @@
 
 import os
+import numpy as np
 
 if not 'I3_SRC' in os.environ:
 	raise RuntimeError("I3_SRC must be defined in the environment! (run env-shell.sh)")
@@ -8,6 +9,12 @@ data_dir = os.path.join(os.environ['I3_SRC'], 'gen2_analysis', 'resources', 'dat
 
 def center(x):
 	return 0.5*(x[1:] + x[:-1])
+
+def edge(x):
+    """ returns bin edges with centers that match x. "inverse" of center(x).
+    """
+    c = center(x)
+    return np.concatenate(([2*x[0]-c[0]], c, [2*x[-1]-c[-1]]))
 
 class constants:
 	annum = 365*24*3600.
