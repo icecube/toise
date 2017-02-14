c925f58af3d64d41a5902a9c08514ad6ab9e2914
jvansanten <jvansanten@16731396-06f5-0310-8873-f7f720988828>
2017-02-14 15:52:39 +0000

    Add GCD to text conversion util
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@153297 16731396-06f5-0310-8873-f7f720988828

---
+++ resources/scripts/data prep/gcd_to_txt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument("gcd_file")
+parser.add_argument("text_file")
+args = parser.parse_args()
+
+import numpy
+from icecube import icetray, dataclasses, dataio
+
+from icecube import icetray, dataio, dataclasses
+f = dataio.I3File(args.gcd_file)
+fr = f.pop_frame(icetray.I3Frame.Geometry)
+f.close()
+
+geo = []
+for key, omgeo in fr['I3Geometry'].omgeo:
+    pos = omgeo.position
+    geo.append([key.string, key.om, pos.x, pos.y, pos.z])
+
+numpy.savetxt(args.text_file, geo, fmt='%d %d %.2f %.2f %.2f')
