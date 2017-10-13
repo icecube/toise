cbb282602f86b359ecab9bde0f2a9de2ccbf3d73
Jakob van Santen <jakob.van.santen@desy.de>
2017-10-13 13:56:06 +0000

    Disable FoMs we don't need for the moment.
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@158654 16731396-06f5-0310-8873-f7f720988828

--- resources/scripts/runall.sh
+++ resources/scripts/runall.sh
@@ -9,26 +9,40 @@ detectors=("--geometry IceCube --veto-area 0"                            \
 "--geometry Sunflower --spacing 200 --veto-area 75 --veto-threshold 1e5" \
 #"--geometry Sunflower --spacing 240 --veto-area 75 --veto-threshold 1e5" \
 "--geometry Sunflower --spacing 300 --veto-area 75 --veto-threshold 1e5" \
-"--geometry EdgeWeighted --spacing 240 --veto-area 75 --veto-threshold 1e5" \
+# "--geometry EdgeWeighted --spacing 240 --veto-area 75 --veto-threshold 1e5" \
 )
 
 figures=(
-"survey_volume"                  \
-"survey_volume --angular-resolution-scale=0.5"                  \
-#"grb"                                     \
-#"diffuse_index --energy-threshold 1e5"    \
-#"diffuse_index --energy-threshold 1e6"    \
-#"galactic_diffuse --energy-threshold 1e4" \
-#"galactic_diffuse --energy-threshold 1e5" \
-#"gzk"                                     \
+# "survey_volume"                  \
+# "survey_volume --angular-resolution-scale=0.5"                  \
+# "grb"                                     \
+"diffuse_index --energy-threshold 1e5"    \
+"diffuse_index --energy-threshold 1e6"    \
+"galactic_diffuse --energy-threshold 1e4" \
+"galactic_diffuse --energy-threshold 1e5" \
+"gzk"                                     \
+)
+
+cascade_figures=(
+"diffuse_index --energy-threshold 1e5"    \
+"diffuse_index --energy-threshold 1e6"    \
+"gzk"                                     \
 )
 
 . ~/.virtualenv/standard/bin/activate 
 
-for f in $figures; do
+# for f in $figures; do
+# 	for d in $detectors; do
+# 		# echo python calculate_fom.py $d $f
+# 		eval python calculate_fom.py $d $f
+# 	done
+# 	# break
+# done
+
+for f in $cascade_figures; do
 	for d in $detectors; do
 		# echo python calculate_fom.py $d $f
-		eval python calculate_fom.py $d $f
+		eval python calculate_fom.py $d $f --cascade-energy-threshold 1e6
 	done
 	# break
 done
