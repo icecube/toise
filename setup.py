c1791d00e9157a5e724781b36302661ec8adfcff
jvansanten <jvansanten@16731396-06f5-0310-8873-f7f720988828>
2018-03-23 10:38:04 +0000

    Sync data files in setup.py, and stash to package dir
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@161833 16731396-06f5-0310-8873-f7f720988828

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from subprocess import check_call
+from os import path
+
+check_call(['rsync', '-avz', '--cvs-exclude', 'data.icecube.wisc.edu:/data/user/jvansanten/projects/2015/gen2_benchmark/data/', path.join(path.dirname(__file__), 'gen2_analysis', 'data')])
 
 setup(name='gen2-analysis',
       version='0.1',
@@ -9,4 +13,5 @@ setup(name='gen2-analysis',
       author_email='jvansanten@icecube.wisc.edu',
       url='http://icecube.wisc.edu/~jvansanten/gen2_analysis/',
       packages=['gen2_analysis'],
+      package_data={'gen2_analysis': ['data/**/*']}
      )
