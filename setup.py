0aac35d22c967c06a7889b86db70468b7f0a6e00
Jakob van Santen <jakob.van.santen@desy.de>
2018-03-23 08:54:29 +0000

    distutils setup script
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@161828 16731396-06f5-0310-8873-f7f720988828

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gen2-analysis',
+      version='0.1',
+      description='IceCube-Gen2 benchmark analysis suite',
+      author='Jakob van Santen',
+      author_email='jvansanten@icecube.wisc.edu',
+      url='http://icecube.wisc.edu/~jvansanten/gen2_analysis/',
+      packages=['gen2_analysis'],
+     )
