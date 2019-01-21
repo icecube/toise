7e66d154b8d376bf6a5834d5ea824aed9d8ea1a3
Jakob van Santen <jakob.van.santen@desy.de>
2019-01-21 15:11:51 +0000

    Make data dir if it does not exist
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@170884 16731396-06f5-0310-8873-f7f720988828

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 from distutils.core import setup
 from subprocess import check_call, PIPE
-from os import path, unlink, environ
+from os import path, unlink, environ, mkdir
 
 if not 'ICECUBE_PASSWORD' in environ:
     raise EnvironmentError('You need to set the environment variable ICECUBE_PASSWORD to the icecube user password.')
 
 cwd = path.join(path.dirname(__file__), 'gen2_analysis', 'data')
+if not path.isdir(cwd):
+    mkdir(cwd)
 check_call(['curl', '--fail', '-u', 'icecube:'+environ['ICECUBE_PASSWORD'], '-O', 'http://convey.icecube.wisc.edu/data/user/jvansanten/projects/2015/gen2_benchmark/data/archive.tar.gz'], cwd=cwd)
 check_call(['tar', 'xzf', 'archive.tar.gz'], cwd=cwd)
 unlink(path.join(path.dirname(__file__), 'gen2_analysis', 'data', 'archive.tar.gz'))
