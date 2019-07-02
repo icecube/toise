c216dc49077f594b32f5bb70df6cabfe1fbcec49
Jakob van Santen <jakob.van.santen@desy.de>
2019-07-02 12:05:21 +0000

    Add conda recipe for nusigma
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@174340 16731396-06f5-0310-8873-f7f720988828

---
+++ conda-recipes/nusigma/setup.py
@@ -0,0 +1,20 @@
+
+from numpy.distutils.core import setup
+from numpy.distutils.core import Extension
+
+setup(
+    name='nusigma',
+    version='1.19',
+    packages=['nusigma'],
+    package_data={
+       'nusigma': ['dat/*.pds','dat/*.tbl'],
+    },
+    ext_modules = [
+        Extension('nusigma._nusigma',
+                  ['_nusigma.pyf', 'dqagse.f', 'd1mach.f'],
+                  extra_objects=[
+                      'lib/libnusigma.a',
+                  ],
+        )
+    ],
+)
