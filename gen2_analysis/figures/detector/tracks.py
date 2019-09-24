02a808f00cc0526b790d406c0e2fcdb928f0a596
Jakob van Santen <jakob.van.santen@desy.de>
2019-09-24 12:15:39 +0000

    Add muon performance plot
    git-svn-id: http://code.icecube.wisc.edu/svn/sandbox/jvansanten/projects/2015/gen2_analysis@175648 16731396-06f5-0310-8873-f7f720988828

---
+++ gen2_analysis/figures/detector/tracks.py
@@ -0,0 +1,89 @@
+
+
+from gen2_analysis.figures import figure
+
+@figure
+def performance():
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from gen2_analysis import plotting, angular_resolution, effective_areas, surfaces, util
+    from scipy.optimize import fsolve, bisect
+
+    @np.vectorize
+    def median_opening_angle(psf, energy, cos_theta):
+        f = lambda psi: psf(psi, energy, cos_theta)-0.5
+        try:
+            return bisect(f, 0, radians(5))
+        except:
+            return np.radians(5)
+
+    psf = angular_resolution.SplineKingPointSpreadFunction()
+    psf_ic = angular_resolution.get_angular_resolution('IceCube')
+    aeff = effective_areas.MuonEffectiveArea('Sunflower', 240)
+
+    loge = np.arange(3.5, 8.5, 0.5) + 0.25
+    loge_centers = 10**util.center(loge)
+
+    fig = plt.figure(figsize=(7, 2.5))
+    griddy = plt.GridSpec(1, 2)
+    ax = plt.subplot(griddy[0])
+    ct = np.linspace(-1, 1, 101)
+    for e in 1e4, 1e5, 1e6, 1e7:
+        line = ax.plot(ct, aeff(e, ct)/1e6, label=plotting.format_energy('%d', e))[0]
+
+    s = surfaces.get_fiducial_surface('Sunflower', 240, padding=0)
+    ax.plot(ct, s.azimuth_averaged_area(ct)/1e6, color='grey', ls=':')
+    x = 0.7
+    y = s.azimuth_averaged_area(x)/1e6
+    ax.annotate(r'$A_{\rm{ geo,IceCube \operatorname{-} Gen2}}$', (x, y,), (-20, 0),
+        textcoords='offset points',
+        ha='right',
+        va='center',
+        arrowprops=dict(arrowstyle='-|>',
+                        facecolor='k',
+                        shrinkA=0.1,
+                        connectionstyle='angle3,angleA=0,angleB=90',
+                        relpos=(1,0.5)
+        )
+    )
+
+    s = surfaces.get_fiducial_surface('IceCube', padding=0)
+    ax.plot(ct, s.azimuth_averaged_area(ct)/1e6, color='grey', ls='--')
+    x = ct[np.argmax(s.azimuth_averaged_area(ct))]
+    y = s.azimuth_averaged_area(x)/1e6
+    ax.annotate(r'$A_{\rm geo, IceCube}$', (x, y,), (20, -20),
+        textcoords='offset points',
+        arrowprops=dict(arrowstyle='-|>',
+                        facecolor='k',
+                        connectionstyle='angle3,angleA=0,angleB=90',
+                        relpos=(0,0.5)
+        )
+    )
+
+    ax.set_ylabel(r'Muon $A_{\rm eff}$ [km$^2$]')
+    ax.set_xlabel(r'$\cos\theta_{\rm zenith}$')
+    ax.set_ylim((0, 8))
+
+    ax = plt.subplot(griddy[1])
+    ctfine = np.linspace(-1, 1, 101)
+    for e in 1e4, 1e5, 1e6, 1e7:
+        line = ax.plot(ctfine, np.degrees(psf.get_quantile(0.5, e, ctfine)), label=plotting.format_energy('%d', e))[0]
+    icmed = np.degrees(median_opening_angle(psf_ic, 1e4, ctfine))
+    ax.plot([-0.4, 0.4], [np.mean(icmed)]*2, ls='--', color='grey')
+    ax.annotate('IceCube 10 TeV', (0, np.mean(icmed)), (0, -3),
+        textcoords='offset points',
+        ha='center',
+        va='top',
+        fontsize='x-small'
+    )
+
+    ax.set_ylabel(r'$\Psi_{\rm median}$ [degrees]')
+    ax.set_xlabel(r'$\cos\theta_{\rm zenith}$')
+    ax.set_ylim((0, 1.0))
+    legend = ax.legend(loc='upper center', title='Muon energy at detector border', ncol=2, fontsize='small', handlelength=1)
+    legend.get_title().set_fontsize('small')
+
+    for ax in fig.axes:
+        ax.set_xlim(-1,1)
+    plt.tight_layout(0.1, w_pad=0.5)
+    return fig
