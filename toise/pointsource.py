fd8cea90ec2e30f4c5d5e69802feaea40152ce96
Steffen Hallmann <steffen.hallmann@desy.de>
2022-04-04 13:39:07 +0200

    muon background dimensions quick fix, maybe better to treat in MuonBackground class differently

--- toise/pointsource.py
+++ toise/pointsource.py
@@ -216,6 +216,38 @@ class WBSteadyPointSource(PointSource):
         PointSource.__init__(self, effective_area, fluence, zenith_bin, with_energy)
         self._livetime = livetime
 
+class NSNSMerger(PointSource):
+    def __init__(self, effective_area, livetime, zenith_bin, with_energy=True):
+        # reference flux is E^2 Phi = 1e-12 TeV^2 cm^-2 s^-1
+        # remember: fluxes are defined as neutrino + antineutrino, so the flux
+        # per particle (which we need here) is .5e-12
+        def intflux(e, gamma):
+            return (e ** (1 + gamma)) / (1 + gamma)
+
+        tev = effective_area.bin_edges[0] / 1e3
+        # 1/cm^2 yr
+        fluence = (
+            0.5e-12
+            * (intflux(tev[1:], -2) - intflux(tev[:-1], -2))
+            * livetime
+            * 365
+            * 24
+            * 3600
+        )
+
+        # scale by the WB GRB fluence, normalized to the E^-2 flux between 100 TeV and 10 PeV
+        from .nsns import NSNS
+
+        norm = (
+            NSNS()(effective_area.bin_edges[0][1:])
+            * effective_area.bin_edges[0][1:] ** 2
+        )
+        norm /= norm.max()
+        fluence *= norm
+
+        PointSource.__init__(self, effective_area, fluence, zenith_bin, with_energy)
+        self._livetime = livetime
+
 
 class TruncatedSteadyPointSource(PointSource):
     def __init__(self, effective_area, livetime, zenith_bin, with_energy=True):
@@ -451,6 +483,11 @@ def discovery_potential(
 def events_above(observables, edges, ecutoff):
     n = 0
     for k, edge_k in edges.items():
+        #print(ecutoff, edges)
+        #print(np.shape(edges))
+        #print(np.shape(observables[k]))
+        if len(edge_k)>2:
+            edge_k = [edge_k[1], edge_k[2]]
         cut = numpy.where(edge_k[1][1:] > ecutoff)[0][0]
         n += observables[k].sum(axis=0)[cut:].sum()
 
