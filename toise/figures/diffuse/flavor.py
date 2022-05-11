aff8a3e90bea8b8fa87991d5e1d174daa7f62905
Jakob van Santen <jvansanten@gmail.com>
2022-05-11 18:01:31 +0200

    Use simplex for flavor triangle
    avoids nondeterministic behavior of L-BFGS-B on M1 with scipy 1.7.3

--- toise/figures/diffuse/flavor.py
+++ toise/figures/diffuse/flavor.py
@@ -164,7 +164,7 @@ def make_profile(
     nominal=dict(),
     suffix="",
     steps=100,
-    minimizer_params=dict(epsilon=1e-2),
+    minimizer_params=dict(tol=None, method="Nelder-Mead"),
     gamma_step=None,
 ):
     if suffix:
@@ -175,7 +175,9 @@ def make_profile(
     fixed = dict(atmo=1, prompt=1, muon=1)
     if "emu_crit" in nominal:
         fixed["emu_crit"] = nominal["emu_crit"]
-    bestfit = fit_llh(llh, **fixed)
+    if gamma_step is None:
+        fixed["gamma"] = llh.components["gamma"].seed
+    bestfit = llh.fit(minimizer_params=minimizer_params, **fixed)
     params = []
     scan = ["mu_fraction" + suffix, "e_tau_ratio" + suffix]
     free = ["astro" + suffix]
