9f746323631f60cd9dbd03816ec70f0816b27100
Jakob van Santen <jvansanten@gmail.com>
2022-06-01 14:32:40 +0200

    Assume no energy resolution for EHE
    rather than perfect energy resolution

--- toise/effective_areas.py
+++ toise/effective_areas.py
@@ -1247,7 +1247,9 @@ def create_gen2_ehe_aeff(
     #         detector with a given energy
     (e_nu, cos_theta, e_det), aeff = _interpolate_gen2_ehe_aeff(cos_theta)
 
-    # Step 2: for now, assume no energy resolution
+    # Step 2: for now, assume _no_ energy resolution
+    e_det = np.array([e_det[0], e_det[-1]])
+    aeff = aeff.sum(axis=-1, keepdims=True)
 
     # Step 3: dummy angular resolution smearing
     psi_bins = numpy.array([0, numpy.inf])
