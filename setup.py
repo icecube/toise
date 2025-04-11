192a24bc6841e1166eef1070f46af7d4bd3d3186
Jakob van Santen <jvansanten@gmail.com>
2025-04-11 14:47:22 +0200

    Update lockfile for micromamba 2.0 (#60)
    * Update lockfile for micromamba 2.0
    
    micromamba 2 wants:
    - package names to be of the form {name}-{version}.{ext}, i.e. install from branch is no longer supported.
    - package hash to be separate from URL
    
    Fixes #59.
    
    * blacken
    
    * Remove obsolete (and now broken) download step
    
    Data files are in git after #52

--- setup.py
+++ setup.py
@@ -1,34 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from os import environ, mkdir, path, unlink
-from subprocess import PIPE, check_call
-
-from setuptools import find_packages
-
-cwd = path.join(path.dirname(__file__), "toise", "data")
-if not path.isdir(cwd):
-    mkdir(cwd)
-
-# if not 'ICECUBE_PASSWORD' in environ:
-#     raise EnvironmentError('You need to set the environment variable ICECUBE_PASSWORD to the icecube user password.')
-# check_call(['curl', '--fail', '-u', 'icecube:'+environ['ICECUBE_PASSWORD'], '-O', 'https://convey.icecube.wisc.edu/data/user/jvansanten/projects/2015/gen2_benchmark/data/archive.tar.gz'], cwd=cwd)
-# check_call(['tar', 'xzf', 'archive.tar.gz'], cwd=cwd)
-# unlink(path.join(path.dirname(__file__), 'toise', 'data', 'archive.tar.gz'))
-
-check_call(
-    [
-        "curl",
-        "--fail",
-        "-o",
-        "minimal-archive.tar.gz",
-        "https://www.zeuthen.desy.de/~jvsanten/toise/minimal-archive.tar.gz",
-        # "https://sandbox.zenodo.org/record/981577/files/minimal-archive.tar.gz?download=1",
-    ],
-    cwd=cwd,
-)
-check_call(["tar", "xzf", "minimal-archive.tar.gz"], cwd=cwd)
-unlink(path.join(path.dirname(__file__), "toise", "data", "minimal-archive.tar.gz"))
+from setuptools import find_packages, setup
 
 setup(
     name="toise",
